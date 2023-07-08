e1b51a28d854bfa295949729a639dc767687f900
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-07-08 21:46:41 +0530

    Add files via upload

---
+++ tutorial10.py
@@ -0,0 +1,22 @@
+# if else and elif
+# conditional statements
+
+
+age = 20
+
+if age >= 18:
+    print("You are an adult.")
+
+else:
+    print("you are not an adult")
+
+
+# elif
+age1 = 25
+
+if age1 < 13:
+    print("You are a child.")
+elif age1 < 18:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
